a336bd3845bca65b3720319d84e393f6d27d7099
ntg-santo <santo@ntg.com.bd>
2024-07-08 12:36:05 +0600

    update

--- FAL_TNA_1008_start.bat
+++ FAL_TNA_1008_start.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "D:\NTG_APP\www\Fashion_Asia_Limited\FAL_TNA"
 
-start /B cmd /C "php artisan serve --host=192.168.100.231:2020"
+start /B cmd /C "php artisan serve --host=192.168.100.231:1008"
 start /B cmd /C "php artisan ser"
